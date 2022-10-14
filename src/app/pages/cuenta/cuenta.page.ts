7706cb3d191dbb64a0b60446c679031c7cde6ced
TopherBsk <jua.silva@duocuc.cl>
2022-10-13 22:53:23 -0300

    Update cuenta.page.ts

--- src/app/pages/cuenta/cuenta.page.ts
+++ src/app/pages/cuenta/cuenta.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-cuenta',
@@ -10,11 +11,20 @@ import { MenuController } from '@ionic/angular';
 export class CuentaPage  {
 
   username: string;
+  
+
   constructor(private router: Router,
     private activatedRouter: ActivatedRoute,
-    private menu: MenuController,) {}
+    private menu: MenuController,
+    private storage:Storage,) {}
 
+  
   ngOnInit() {
+    this.vernombre();
   }
 
+  async vernombre()
+  {
+    this.username=await this.storage.get('sesion');
+  }
 }
