9db532ece02275f0b9259553ffa41c44c90c56c0
Blanca Hidalgo <101121971+Blanca12luz@users.noreply.github.com>
2022-12-01 21:57:22 -0300

    me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
 
   {
     path: '',
-    redirectTo: 'Loginpage',
+    redirectTo: 'loginpage',
     pathMatch: 'full'
   },
 
@@ -16,7 +16,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
-    
+    canActivate:[AutorizarGuard]
   },
   {
     path: 'loginpage',
